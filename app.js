065cccab34ea76881e59a9a60f02adc8cf7f8093
Casey Rieder <casey.rieder@gmail.com>
2016-01-21 02:12:58 -0500

    made template dynamic

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 if (Meteor.isClient) {
 	angular.module('socially', ['angular-meteor']);
+
+	angular.module('socially').controller('PartiesListCtrl', function ($scope) {
+		$scope.parties = [
+			{
+				'name': 'Dubstep-Free Zone',
+				'description': 'Can we PLEASE just for an evening not listen to dubstep!?!?!?'
+			},
+			{
+				'name': 'All Dubstep All the Time',
+				'description': 'Get it on!'
+			},
+			{
+				'name': 'Savage Lounging',
+				'description': 'Leisure suit required. And only fiercest manners'
+			}
+		];
+	});
 }
