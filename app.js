f968eddce98ef4abd5b42407b1491c847d4c9c20
Casey Rieder <casey.rieder@gmail.com>
2016-01-23 14:55:41 -0500

    Added event handling to insert & remove parties

--- app.js
+++ app.js
@@ -10,12 +10,23 @@ if (Meteor.isClient) {
 			controllerAs: 'partiesList',
 			controller: function($scope, $reactive) {
 				$reactive(this).attach($scope);
-
+				// Declare newParty variable
+				this.newParty = {};
+				// List parties from Collection
 				this.helpers({
 					parties: () => {
 						return Parties.find({});
 					}
 				});
+				// Insert party into Collection
+				this.addParty = () => {
+					Parties.insert(this.newParty);
+					this.newParty = {};
+				};
+				// Delete party from Collection
+				this.removeParty = (party) => {
+					Parties.remove({_id: party._id});
+				};
 			}
 		}
 	});
