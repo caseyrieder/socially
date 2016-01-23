e9a089e34fe96077451558878d961a2c8363c494
Casey Rieder <casey.rieder@gmail.com>
2016-01-22 19:48:22 -0500

    Finished 3-way binding, built component for parties list controller

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
 	angular.module('socially', ['angular-meteor']);
 
-	angular.module('socially').controller('PartiesListCtrl', function ($scope) {
-		$scope.parties = [
-			{
-				'name': 'Dubstep-Free Zone',
-				'description': 'Can we PLEASE just for an evening not listen to dubstep!?!?!?'
-			},
-			{
-				'name': 'All Dubstep All the Time',
-				'description': 'Get it on!'
-			},
-			{
-				'name': 'Savage Lounging',
-				'description': 'Leisure suit required. And only fiercest manners'
+	angular.module('socially').directive('partiesList', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'parties-list.html'
+			controllerAs: 'partiesList',
+			controller: function($scope, $reactive) {
+				$reactive(this).attach($scope);
+
+				this.helpers({
+					parties: () => {
+						return Parties.find({});
+					}
+				});
 			}
-		];
+		}
 	});
 }
