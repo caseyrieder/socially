8ac7d84fa9d27c6b641beffb15e0e957084924df
Casey Rieder <casey.rieder@gmail.com>
2016-01-25 15:31:03 -0500

    Added party details view, made each party updatable

--- app.js
+++ app.js
@@ -61,8 +61,30 @@ if (Meteor.isClient) {
 			restrict: 'E',
 			templateUrl: 'party-details.html',
 			controllerAs: 'partyDetails',
-			controller: function($scope, $stateParams) {
-				this.partyId = $stateParams.partyId;
+			controller: function($scope, $stateParams, $reactive) {
+				$reactive(this).attach($scope);
+				//Find the appropriate party
+				this.helpers({
+					party: () => {
+						return Parties.findOne({_id: $stateParams.partyId});
+					}
+				});
+				//Save/update the name & description of the current party
+				this.save = () => {
+					Parties.update({_id: $stateParams.partyId}, {
+						$set: {
+							name: this.party.name,
+							description: this.party.description
+						}
+					}, (error) => {
+						if (error) {
+							console.log("Oops, unable to update the party...");
+						} else {
+							console.log("Done!")
+						}
+					});
+				};
+
 			}
 		}
 	});
